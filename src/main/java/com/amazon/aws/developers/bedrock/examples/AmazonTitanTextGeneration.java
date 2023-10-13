e07373bb64f7a35ef2c403df711eeb12f700af73
Ricardo Ferreira <riferrei@riferrei.com>
2023-10-13 08:23:31 -0400

    Fixed identation

--- src/main/java/com/amazon/aws/developers/bedrock/examples/AmazonTitanTextGeneration.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/AmazonTitanTextGeneration.java
@@ -16,13 +16,13 @@ public class AmazonTitanTextGeneration {
     private static final String MODEL_ID = "amazon.titan-tg1-large";
 
     private static final String PROMPT = """
-            Extract the band name from the contract:
+        Extract the band name from the contract:
 
-            This Music Recording Agreement ("Agreement") is made effective as of the 13 day of December,
-            2021 by and between Good Kid, a Toronto-based musical group (“Artist”) and Universal Music Group,
-            a record label with license number 545345 (“Recording Label"). Artist and Recording Label may each
-            be referred to in this Agreement individually as a "Party" and collectively as the "Parties."
-            Work under this Agreement shall begin on March 15, 2022.
+        This Music Recording Agreement ("Agreement") is made effective as of the 13 day of December,
+        2021 by and between Good Kid, a Toronto-based musical group (“Artist”) and Universal Music Group,
+        a record label with license number 545345 (“Recording Label"). Artist and Recording Label may each
+        be referred to in this Agreement individually as a "Party" and collectively as the "Parties."
+        Work under this Agreement shall begin on March 15, 2022.
     """;
 
     public static void main(String... args) throws Exception {
