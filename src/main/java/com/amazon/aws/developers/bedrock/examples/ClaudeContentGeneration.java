f738c08f5d2bf8e130a4830003dc436fecc9be96
Ricardo Ferreira <riferrei@riferrei.com>
2023-10-12 20:42:29 -0400

    Changed the chat/content examples

--- src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
+++ src/main/java/com/amazon/aws/developers/bedrock/examples/ClaudeContentGeneration.java
@@ -16,14 +16,22 @@ public class ClaudeContentGeneration {
     private static final String MODEL_ID = "anthropic.claude-v2";
 
     private static final String PROMPT = """
-        <paragraph> 
-            "In 1758, the Swedish botanist and zoologist Carl Linnaeus published in his Systema Naturae,
-            the two-word naming of species (binomial nomenclature). Canis is the Latin word meaning "dog",
-            and under this genus, he listed the domestic dog, the wolf, and the golden jackal."
-        </paragraph>
+        Generative AI refers to artificial intelligence systems that are capable of generating
+        novel content such as text, images, audio, video, and more, as opposed to just analyzing
+        existing data. The key aspect of generative AI is that it creates brand new outputs that
+        are unique and original, not just reproductions or remixes of existing content. Generative
+        AI leverages machine learning techniques like neural networks that are trained on large
+        datasets to build an understanding of patterns and structures within the data. It then
+        uses that knowledge to generate new artifacts that conform to those patterns but are not
+        identical copies, allowing for creativity and imagination. Prominent examples of generative
+        AI include systems like DALL-E that creates images from text descriptions, GPT-3 that
+        generates human-like text, and WaveNet that produces realistic synthetic voices. Generative
+        models hold great promise for assisting and augmenting human creativity across many domains
+        but also raise concerns about potential misuse if not thoughtfully implemented. Overall,
+        generative AI aims to mimic human creative abilities at scale to autonomously produce
+        high-quality, diverse, and novel content.        
 
-        Please rewrite the above paragraph to make it understandable to a 5th grader.
-        Please output your rewrite in <rewrite></rewrite> tags.
+        Please create a single short paragraph so that a fiver years old child can understand.
     """;
 
     public static void main(String... args) throws Exception {
